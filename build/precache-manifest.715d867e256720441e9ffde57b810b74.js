a22beb4cbec2570c3921a005d6c1ac843167194a
Diane L. Jones <dlj.co27@gmail.com>
2018-11-14 10:47:49 -0500

    build updated

---
+++ build/precache-manifest.715d867e256720441e9ffde57b810b74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "863c3b6949c4a31ef10d",
+    "url": "/Jonesdl-2785/myReads-FEND/static/css/main.383edd12.chunk.css"
+  },
+  {
+    "revision": "863c3b6949c4a31ef10d",
+    "url": "/Jonesdl-2785/myReads-FEND/static/js/main.863c3b69.chunk.js"
+  },
+  {
+    "revision": "4c56f981ed9b9e167f41",
+    "url": "/Jonesdl-2785/myReads-FEND/static/js/1.4c56f981.chunk.js"
+  },
+  {
+    "revision": "f454bce5766db6b2ae51",
+    "url": "/Jonesdl-2785/myReads-FEND/static/js/runtime~main.f454bce5.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/Jonesdl-2785/myReads-FEND/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/Jonesdl-2785/myReads-FEND/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/Jonesdl-2785/myReads-FEND/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "8116573fae776cd0ed84acb5ea5c3619",
+    "url": "/Jonesdl-2785/myReads-FEND/static/media/rebel.8116573f.png"
+  },
+  {
+    "revision": "1002c4fdf1de89e1fbc6902f6c02f3aa",
+    "url": "/Jonesdl-2785/myReads-FEND/static/media/natural-paper.1002c4fd.png"
+  },
+  {
+    "revision": "9ba2e85fd65881ff40e91b302e8509d6",
+    "url": "/Jonesdl-2785/myReads-FEND/index.html"
+  }
+];
